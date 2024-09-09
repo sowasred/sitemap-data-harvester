7cbefb77db128879411a5095e913e3e9ceba985e
Ozan <muldurozan@gmail.com>
2024-09-09 00:24:11 -0400

    default port changed and cors enabled

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(cors());
 
 app.use('/api', routes);
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
