b85213665c29da2932cde825c2c5041c9a144306
Ozan <muldurozan@gmail.com>
2024-09-26 11:41:34 -0400

    Limit request body size to prevent potential DoS attacks

--- src/server.js
+++ src/server.js
@@ -6,7 +6,9 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json());
+app.use(express.json({ limit: '10kb' })); // Limiting JSON payload size to 10kb
+app.use(express.urlencoded({ extended: true, limit: '10kb' })); // Limiting URL-encoded payload size
+
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(cors());
 
